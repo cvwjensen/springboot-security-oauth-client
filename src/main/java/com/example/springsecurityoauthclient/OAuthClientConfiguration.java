b0112aa0790f3d876c57edfc399fd9acbbe7bf34
cvwj <cvwjensen@gmail.com>
2023-12-14 06:42:54 +0100

    Update to 3.2.0

--- src/main/java/com/example/springsecurityoauthclient/OAuthClientConfiguration.java
+++ src/main/java/com/example/springsecurityoauthclient/OAuthClientConfiguration.java
@@ -18,7 +18,8 @@ import java.util.List;
 public class OAuthClientConfiguration {
 
     @Bean
-    // Register the client registration with the authorization server
+        // Register the client registration with the authorization server
+        // See the well-known config endpoint here http://localhost:8080/realms/springboot/.well-known/openid-configuration
     ReactiveClientRegistrationRepository clientRegistrations(
             @Value("${spring.security.oauth2.client.provider.external.token-uri}") String token_uri,
             @Value("${spring.security.oauth2.client.registration.external.client-id}") String client_id,
@@ -39,16 +40,25 @@ public class OAuthClientConfiguration {
     }
 
     @Bean
-    //    Create a WebClient that is aware of the OAuth2 token and uses the 'external' client to enrich the request with an AccessToken
     WebClient webClient(ReactiveClientRegistrationRepository clientRegistrations) {
+        // An OAuth2AuthorizedClientService that stores Authorized Client(s) in-memory.
         InMemoryReactiveOAuth2AuthorizedClientService clientService = new InMemoryReactiveOAuth2AuthorizedClientService(clientRegistrations);
+
+        // An OAuth2AuthorizedClientManager that uses an OAuth2AuthorizedClientService to manage OAuth 2.0 Authorized Client(s).
         AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager authorizedClientManager = new AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(clientRegistrations, clientService);
+
+        // Provides an easy mechanism for using an OAuth2AuthorizedClient to make OAuth2 requests by including the token as a Bearer Token.
         ServerOAuth2AuthorizedClientExchangeFilterFunction oauth = new ServerOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+
+        // Set the default ClientRegistration.Id that will be used for requests.
         oauth.setDefaultClientRegistrationId("external");
+
+        // Create a WebClient that is aware of the OAuth2 token and uses the 'external' client to enrich the request with an AccessToken
         return WebClient.builder()
                 .filter(oauth)
                 .build();
 
     }
+
 
 }
